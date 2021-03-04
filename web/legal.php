9f6f60b033493fb9681642f064485f76f0380b4a
Dominic Price <dominic.price@nottingham.ac.uk>
2021-03-04 22:53:43 +0000

    Update theme

--- web/legal.php
+++ web/legal.php
@@ -6,4 +6,35 @@ require_once "version.inc";
 
 print_header($context);
 
+?>
+
+<h1 id="legal">Legal</h1>
+<p>
+The <a href="https://cobotmakerspace.org" title="https://cobotmakerspace.org">
+Cobot Maker Space</a> is part of the
+<a href="https://www.nottingham.ac.uk" title="https://www.nottingham.ac.uk">
+University of Nottingham</a>, please refer to the legal notices on the
+<a href="https://www.nottingham.ac.uk" title="https://www.nottingham.ac.uk">
+University of Nottingham website</a>.
+</p>
+
+<h2 id="privacy_policy">Privacy Policy</h2>
+<p>
+Please refer to the privacy policy on the
+<a
+    href="https://www.nottingham.ac.uk/utilities/privacy/privacy.aspx"
+    title="https://www.nottingham.ac.uk/utilities/privacy/privacy.aspx">
+  University of Nottingham website</a>.
+</p>
+
+<h2 id="cookie_policy">Cookie Policy</h2>
+<p>
+Please refer to the cookie policy on the
+<a
+    href="https://www.nottingham.ac.uk/utilities/cookies.aspx"
+    title="https://www.nottingham.ac.uk/utilities/cookies.aspx">
+  University of Nottingham website</a>.
+</p>
+
+<?php
 print_footer();
